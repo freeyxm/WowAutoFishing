bbd1120d3786d3554066a1dea434279717d79c9d
freeyxm <freeyxm@gmail.com>
2016-04-04 15:14:26 +0800

    Update CommUtil.

--- CommUtil/StringUtil.cpp
+++ CommUtil/StringUtil.cpp
@@ -17,4 +17,23 @@ float StringUtil::atof(const char *str)
 	return (float)::atof(str);
 }
 
+std::list<std::string> StringUtil::split(const char *str, const char *delimiter)
+{
+	list<string> strs;
+	size_t size = strlen(str);
+	std::shared_ptr<char> buf(new char[size + 1], std::default_delete<char[]>());
+	assert(buf.get());
+
+	buf.get()[0] = '\0';
+	memcpy_s(buf.get(), size + 1, str, size);
+
+	char *ptr = nullptr, *next = buf.get();
+	while ((ptr = strtok_s(next, delimiter, &next)) != nullptr)
+	{
+		strs.push_back(string(ptr));
+	}
+
+	return strs;
+}
+
 } // namespace comm_util
--- CommUtil/StringUtil.h
+++ CommUtil/StringUtil.h
@@ -1,9 +1,15 @@
 #pragma once
 #include <cstring>
+#include <string>
 #include <vector>
+#include <list>
+#include <memory>
+#include <assert.h>
 
 namespace comm_util
 {
+
+using namespace std;
 
 class StringUtil
 {
@@ -13,6 +19,8 @@ public:
 
 	static float atof(const char *str);
 
+	static std::list<std::string> split(const char *str, const char *delimiter);
+
 	template<typename T>
 	static std::vector<T> parseValues(const char *str, const char *delimiter, T(*convert)(const char*));
 };
@@ -21,10 +29,14 @@ template<typename T>
 static std::vector<T> StringUtil::parseValues(const char *str, const char *delimiter, T(*convert)(const char*))
 {
 	std::vector<T> data;
-	char buf[10240] = { 0 };
-	memcpy_s(buf, sizeof(buf), str, strlen(str));
+	size_t size = strlen(str);
+	std::shared_ptr<char> buf(new char[size + 1], std::default_delete<char[]>());
+	assert(buf.get());
 
-	char *ptr = nullptr, *next = buf;
+	buf.get()[0] = '\0';
+	memcpy_s(buf.get(), size + 1, str, size);
+
+	char *ptr = nullptr, *next = buf.get();
 	while ((ptr = strtok_s(next, delimiter, &next)) != nullptr)
 	{
 		data.push_back((*convert)(ptr));
